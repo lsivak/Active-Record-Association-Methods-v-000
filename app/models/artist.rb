17ade956e2d87e666f108ca29b63d22c5a44998d
Lara Sanders <larasanders@gmail.com>
2017-12-08 00:15:12 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,9 +3,13 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 #return the genre of the artist's first saved song
   def self.get_genre_of_first_song
-    song = Song.new(name: song)
-    artist = Artist.create(name: artist)
-
+    song = Artist.create(name: song)
+    song.artist = artist
+    song_title = Song.create(name: song_title)
+    artist.songs << song_title
+    song_title.artist
+    song.genre = Genre.create(name: genre)
+    song.artist.genre = song.genre
   end
 
   def song_count
