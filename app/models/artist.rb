3bee682d998760492a276c3aea0793f190b2458d
Lara Sanders <larasanders@gmail.com>
2017-12-08 15:24:40 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,12 +2,8 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 #return the genre of the artist's first saved song
-
-
-
-
   def get_genre_of_first_song
-self.songs.first.genre
+    self.songs.first.genre
   end
 
   def song_count
