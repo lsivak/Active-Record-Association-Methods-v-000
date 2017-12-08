598f57fff80fccbfc5ae734da2861d25962dda43
Lara Sanders <larasanders@gmail.com>
2017-12-08 15:40:08 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,7 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    artists.all.each do |name|
+    artists.all.collect do |name|
       artists.name
     # return an array of strings containing every musician's name
   end
